672f952d1f69fbf92e335793166c59b0536ca428
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-22 23:34:03 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -6,7 +6,7 @@ load_dotenv("./.env")
 
 
 class Config:
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "2119246346:AAFRYFzFIi4ibCxlsukcULuLPH0NqPEtvHo")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6420144202:AAF_vM9N9DTmoW5oII4RcxvfZYfD7swIxvU")
   BOT_NAME = os.environ.get("BOT_NAME", "Aleena")
 
   API_ID = int(os.environ.get("API_ID", "15851949"))
