d40da8d106bd6a7f676211e2d1ff61b61578a280
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-02-08 22:01:18 +0530

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -5,14 +5,13 @@ from dotenv import load_dotenv
 load_dotenv("./.env")
 
 
-class Config:
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6420144202:AAF_vM9N9DTmoW5oII4RcxvfZYfD7swIxvU")
+class Config:xvfZYfD7swIxvU")
   BOT_NAME = os.environ.get("BOT_NAME", "Aleena")
 
   API_ID = int(os.environ.get("API_ID", "15851949"))
   API_HASH = os.environ.get("API_HASH", "f8e386978bf103c4a6baab0a4b92e822")
 
-  DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb://cls5zmw31000kaume2jhqcdpd:y9q9BOZGYBTmvGdXZFyUcj8A@104.251.218.202:9001/?readPreference=primary&ssl=false")
+  DATABASE_URI = os.environ.get("DATABASE_URI", "mongodbBTmvGdXZFyUcj8A@104.2eadPreference=primary&ssl=false")
   SESSION_NAME = os.environ.get("DATABASE_NAME", "Aleena")
 
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -1002049466903))
